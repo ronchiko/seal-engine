2cf6b18aea76bde8dca55f7921bf6ecbfac9aed5
Ron Horowitz <get.snoop.scoped@gmail.com>
2022-04-20 13:30:14 +0300

    Add utility functions

--- include/seal/math.h
+++ include/seal/math.h
@@ -2,17 +2,24 @@
 
 #include <float.h>
 
+#define CREATE_SWAP_FUNCTION(type, extension) \
+static inline void Seal_Swap##extension(type *a, type *b) {type t = *a; *a = *b; *b = t; } 
+
+#define CREATE_MIN_MAX_FUNCTIONS(type, extension) \
+static inline type Seal_Max##extension(type a, type b) { return a < b ? b : a; } \
+static inline type Seal_Min##extension(type a, type b) { return a > b ? b : a; }
+
 /* Linear interpolation (0 <= t <= 1) */
 static inline float Seal_Lerp(float a, float b, float t) { return b * t + a * (1 - t); }
 
 float Seal_Clampf(float v, float min, float max);
 int Seal_Clamp(int v, int min, int max);
 
-static inline int Seal_Max(int a, int b) { return a < b ? b : a; }
-static inline int Seal_Min(int a, int b) { return a > b ? b : a; }
+CREATE_MIN_MAX_FUNCTIONS(int, );
+CREATE_SWAP_FUNCTION(int, );
 
-static inline float Seal_Maxf(float a, float b) { return a < b ? b : a; }
-static inline float Seal_Minf(float a, float b) { return a > b ? b : a; }
+CREATE_MIN_MAX_FUNCTIONS(float, f);
+CREATE_SWAP_FUNCTION(float, f);
 
 /* Rounds to the nearest integer */
 float Seal_Round(float a);
@@ -29,6 +36,8 @@ float Seal_FastInverseSqrt(float n);
 #define SEAL_MAX_FLOAT		FLT_MAX
 #define SEAL_MIN_FLOAT		FLT_MIN
 #define SEAL_EPSILON		FLT_EPSILON
+
+#define Seal_IsBetween(min, n, max) ((min) <= (n) && (n) <= (max))  
 
 // Additional maths
 #include "seal/math/vector2.h"
